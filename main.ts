838fc8921022e2689087252948425fed361c36a8
Marcos Candeia <marrcooos@gmail.com>
2024-05-06 20:09:53 -0300

    Add punchserver
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- main.ts
+++ main.ts
@@ -1,10 +1,8 @@
-/// <reference no-default-lib="true"/>
-/// <reference lib="dom" />
-/// <reference lib="deno.ns" />
-/// <reference lib="esnext" />
+import { PunchmoleServer } from "npm:punchmole";
 
-import { start } from "$fresh/server.ts";
-import config from "./fresh.config.ts";
-import manifest from "./fresh.gen.ts";
-
-await start(manifest, config);
+PunchmoleServer(
+  Deno.env.get("PORT") ?? 8000, // port number to listen on
+  Deno.env.get("API_KEYS")?.split(",") ?? [], // array of api keys (random strings)
+  "/_punchmole", // /_punchmole is the default path
+  console, // console, {info: {}, debug: {}, error: {}} for no logs or e.g. an instance of log4js
+);
