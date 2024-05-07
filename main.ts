51b4236aac9903ba86a7146ead45253894ce6998
Marcos Candeia <marrcooos@gmail.com>
2024-05-07 07:30:47 -0300

    Use mcandeia fork
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { PunchmoleServer } from "npm:punchmole";
+import { PunchmoleServer } from "https://esm.sh/gh/mcandeia/punchmole@3f96d0c4da5211c357b9eae895775053c32a0b1b/PunchmoleServer.js";
 
 PunchmoleServer(
   Deno.env.get("PORT") ?? 8000, // port number to listen on
