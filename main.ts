9e3cb05dd7eaf15831f79c4eb658b90fa10037b3
Marcos Candeia <marrcooos@gmail.com>
2024-05-22 09:20:07 -0300

    Use punchmole deno version
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
-import { PunchmoleServer } from "https://esm.sh/gh/mcandeia/punchmole@e295d48d6451934612ab6747b63751fa0dd7d6fe/PunchmoleServer.js";
+import { PunchmoleServer } from "https://denopkg.com/mcandeia/punchmole@02041f4a9087703e072879f99c6501ac90af9c6b/PunchmoleServer.ts";
 
+const port = Deno.env.get("PORT");
 PunchmoleServer(
-  Deno.env.get("PORT") ?? 8000, // port number to listen on
+  port ? +port : 8000, // port number to listen on
   Deno.env.get("API_KEYS")?.split(",") ?? [], // array of api keys (random strings)
   "/_punchmole", // /_punchmole is the default path
   console, // console, {info: {}, debug: {}, error: {}} for no logs or e.g. an instance of log4js
